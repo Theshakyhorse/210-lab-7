82a89845d9c23244771dd37c72394116d95aee82
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-15 18:38:48 -0800

    third

--- main.cpp
+++ main.cpp
@@ -2,21 +2,40 @@
 #include <iostream>
 using namespace std;
 
-string* reverseArray(string *Arr);
+string* reverseArray(string *);
+void displayArray(string *);
 
 int main() {
     string *sptr = nullptr;
     sptr = new string[5];
+
     sptr[0] = "Alexander";
     sptr[1] = "Max";
     sptr[2] = "Sierra";
     sptr[3] = "Alex";
     sptr[4] = "Maximus";
-    cout << "Original array: " << endl;
+
+    cout << "Original array: ";
+    displayArray(sptr);
+
+    cout << "Reversed array: ";
+    displayArray(reverseArray(sptr));
     return 0;
 }
 
 string* reverseArray(string *Arr){
-    
+    string temp;
+    for(int i = 0; i < 5; i++){
+        temp = Arr[i];
+        Arr[i] = Arr[5-i];
+        Arr[5-i] = temp;
+    }
     return Arr;
+}
+
+void displayArray(string *Arr){
+    for(int i = 0; i < 5; i++){
+        cout << *(Arr + i) << " ";
+    }
+    cout << endl;
 }
