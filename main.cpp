d581f63d674dfe4eb4291ab44f7deda5a659357d
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-15 18:28:47 -0800

    second

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <iostream>
 using namespace std;
 
+string* reverseArray(string *Arr);
+
 int main() {
-    
+    string *sptr = nullptr;
+    sptr = new string[5];
+    sptr[0] = "Alexander";
+    sptr[1] = "Max";
+    sptr[2] = "Sierra";
+    sptr[3] = "Alex";
+    sptr[4] = "Maximus";
+    cout << "Original array: " << endl;
     return 0;
+}
+
+string* reverseArray(string *Arr){
+    
+    return Arr;
 }
